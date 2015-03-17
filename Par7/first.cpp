2d18e29fc1d3dcfca8bde1a340fdf94d3b5c6e59
Yuriy Solodkyy <yuriy.solodkyy@gmail.com>
2015-03-16 23:57:31 -0700

    More code for First set computation

--- Par7/first.cpp
+++ Par7/first.cpp
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 CrowdsTech. All rights reserved.
 //
 
+#include <map>
 #include <set>
 #include <string>
 #include "ebnfdefs.hpp"
@@ -56,6 +57,35 @@ std::set<std::string> nonterminals(const Syntax& grammar)
     {
         std::set<std::string> tmp = nonterminals(rule);
         result.insert(tmp.begin(), tmp.end());
+    }
+
+    return result;
+}
+
+std::set<const Terminal*> first(const Def& def)
+{
+    return std::set<const Terminal*>();
+}
+
+std::map<std::string, std::set<const Terminal*>> first(const Syntax& grammar)
+{
+    std::map<std::string, std::set<const Terminal*>> result;
+    std::set<std::string> keys = nonterminals(grammar);
+
+    for (const auto& n : keys)
+    {
+        result[n] = std::set<const Terminal*>();
+    }
+
+    while (true)
+    {
+        for (const auto& rule : grammar)
+        {
+            for (const auto& def : static_cast<const Alternatives&>(rule))
+            {
+                std::set<const Terminal*> f = first(def);
+            }
+        }
     }
 
     return result;
